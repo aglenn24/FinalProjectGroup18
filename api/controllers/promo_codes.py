e10ab02282729361b6ab98040877ad8a2e9d74b2
Leo Barguti <93502924+lbarguti@users.noreply.github.com>
2024-11-22 14:55:42 -0500

    Finished Controllers

--- api/controllers/promo_codes.py
+++ api/controllers/promo_codes.py
@@ -6,7 +6,6 @@ from sqlalchemy.exc import SQLAlchemyError
 
 def create(db: Session, request):
     new_item = model.PromoCodes(
-        id=request.id,
         promo=request.promo,
         expiration=request.expiration
     )
@@ -33,7 +32,7 @@ def read_all(db: Session):
 
 def read_one(db: Session, item_id):
     try:
-        item = db.query(model.PromoCodes).filter(model.PromoCodes.promo == item_id).first()
+        item = db.query(model.PromoCodes).filter(model.PromoCodes.id == item_id).first()
         if not item:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
     except SQLAlchemyError as e:
@@ -44,7 +43,7 @@ def read_one(db: Session, item_id):
 
 def update(db: Session, item_id, request):
     try:
-        item = db.query(model.PromoCodes).filter(model.PromoCodes.promo == item_id)
+        item = db.query(model.PromoCodes).filter(model.PromoCodes.id == item_id)
         if not item.first():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
         update_data = request.dict(exclude_unset=True)
@@ -58,7 +57,7 @@ def update(db: Session, item_id, request):
 
 def delete(db: Session, item_id):
     try:
-        item = db.query(model.PromoCodes).filter(model.PromoCodes.promo == item_id)
+        item = db.query(model.PromoCodes).filter(model.PromoCodes.id == item_id)
         if not item.first():
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
         item.delete(synchronize_session=False)
