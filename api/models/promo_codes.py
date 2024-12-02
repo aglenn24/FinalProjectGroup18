0951fe32e201e2d12e38e4f5c54f90fd133c0fe4
aglenn24 <aglenn24@charlotte.edu>
2024-12-02 18:44:24 -0500

    Added discount_percent

--- api/models/promo_codes.py
+++ api/models/promo_codes.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME, CheckConstraint
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from ..dependencies.database import Base
@@ -9,4 +9,7 @@ class PromoCodes(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     promo = Column(String(25), unique=True, nullable=False)
+    discount_percent = Column(DECIMAL(3, 2), CheckConstraint("discount_percent >= 0 AND discount_percent <= 100"), nullable=False)
     expiration = Column(DATETIME, unique=False, nullable=False)
+
+    orders = relationship("Order", back_populates="promo_code")
