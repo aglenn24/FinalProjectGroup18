a2546fbd853525550df1c523d65df62e6635a77b
Liam McCracken <lmccrac2@uncc.edu>
2024-12-05 18:44:46 -0500

    reversed changes to order and order detail models

--- api/models/order_details.py
+++ api/models/order_details.py
@@ -11,20 +11,8 @@ class OrderDetail(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     order_id = Column(Integer, ForeignKey("orders.id"))
     
-    # customer info
-    customer_name = Column(String(100))
-    customer_address = Column(String(100))
-    customer_email = Column(String(50))
-    customer_phone = Column(String(20))
-    description = Column(String(300))
-    
-    # order info
-    tracking_number = Column(Integer, tracking_number_sequence, server_default=tracking_number_sequence.next_value(), unique = True)
-    order_status = Column(String(20), nullable=False, server_default='0.0')
-    order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
-    
-    #review
-    review_text = Column(String(500))
-    score = Column(DECIMAL(2, 1), CheckConstraint("score >= 1 AND score <= 5", name="valid_score"))
+    # order contents
+    sandwich_id = Column(Integer, ForeignKey("sandwiches.id"))
+    amount = Column(Integer, index=True, nullable=False)
     
     order = relationship("Order", back_populates="order_details")
--- api/models/orders.py
+++ api/models/orders.py
@@ -1,18 +1,30 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME, Sequence
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME, CheckConstraint, Sequence
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from ..dependencies.database import Base
 
-
+tracking_number_sequence = Sequence('tracking_number_seq', start=00000, increment=1)
 
 class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+
+    # customer info
+    customer_name = Column(String(100))
+    customer_address = Column(String(100))
+    customer_email = Column(String(50))
+    customer_phone = Column(String(20))
+    description = Column(String(300))
     
-    # order contents
-    sandwich_id = Column(Integer, ForeignKey("sandwiches.id"))
-    amount = Column(Integer, index=True, nullable=False)
+    # order info
+    tracking_number = Column(Integer, tracking_number_sequence, server_default=tracking_number_sequence.next_value(), unique = True)
+    order_status = Column(String(20), nullable=False, server_default='0.0')
+    order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
+    
+    #review
+    review_text = Column(String(500))
+    score = Column(DECIMAL(2, 1), CheckConstraint("score >= 1 AND score <= 5", name="valid_score"))
     
     # payment info
     card_info = Column(String(25), nullable=True)  
@@ -21,6 +33,9 @@ class Order(Base):
         nullable=False,
         default="Pending"
     )
+    
+    total_price = Column(DECIMAL(4, 2), nullable=False, server_default='0.0')
+    
     payment_type = Column(String(50), nullable=True)  
     
     order_details = relationship("OrderDetail", back_populates="order")
